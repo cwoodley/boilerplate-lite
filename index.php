ddc9d3d2bc7fd790cbdc7c6192f71ea210b62812
Cale Woodley <cale@blackboxdesign.com.au>
2010-08-31 17:16:24 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <title></title>
+  <!-- iPhone Icon -->
+  <!-- <link rel="apple-touch-icon" href="media/images/iphone-icon.png" /> -->
+
+  <!-- http://code.google.com/p/ie7-js/ -->
+  <!--[if lt IE 8]>
+  <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta3)/IE8.js"></script>
+  <![endif]-->
+
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="media/stylesheets/compiled/reset.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="media/stylesheets/compiled/text.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="media/stylesheets/compiled/style.css" media="all" />
+
+</head>
+
+<body>
+
+
+<!-- JS -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" src="media/javascripts/presentation.js"></script>
+</body>
+</html>
